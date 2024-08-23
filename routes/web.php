a0743630a979dcfdbbcdb7c1c035c3cc81926ce1
Muhamad Aldizar Ilham <ilhamaldizar@gmail.com>
2024-08-23 13:21:07 +0700

    added login, register and forget password

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/app', function () {
+    return view('Backend.dashboard');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/register', function () {
+    return view('auth.ragister');
+});
+
+Route::get('/forget', function () {
+    return view('auth.forget');
 });
